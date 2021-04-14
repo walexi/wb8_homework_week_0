e11f9116234e7dc2fb7641b501f220700ebdace9
Olawale Onabola <walexi4great@gmail.com>
2021-04-13 20:29:52 -0400

    sort array by parity solution - constant space complexity

--- sort_array_by_parity.py
+++ sort_array_by_parity.py
@@ -9,3 +9,15 @@ class Solution:
                 q.append(num)
                 
         return list(q)
+
+    def solution2(self, A: List[int]) -> List[int]:
+        start=0
+        end=len(A)-1
+        
+        while start<end:
+            if A[start]%2==0:
+                start+=1
+            else:#swap with the end pointer
+                A[end], A[start]=A[start], A[end]
+                end-=1
+        return A
